8e2d2fd938d6b0b738bcf55947c309b0f75d9cb4
Teo03 <39939408+Teodor03@users.noreply.github.com>
2019-06-17 18:05:07 +0300

    Add files via upload

---
+++ B12014.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+const int Max_size=10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+ios_base::sync_with_stdio(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
